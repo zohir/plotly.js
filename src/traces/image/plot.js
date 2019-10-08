3567b6282842ca2c26e27e8117853c26a86a2f99
Antoine Roy-Gobeil <antoine@plot.ly>
2019-10-08 15:50:06 -0400

    image: turn off image smoothing for other browsers

--- src/traces/image/plot.js
+++ src/traces/image/plot.js
@@ -63,8 +63,8 @@ module.exports = function(gd, plotinfo, cdimage, imageLayer) {
 
         // TODO: support additional smoothing options
         // https://developer.mozilla.org/en-US/docs/Web/CSS/image-rendering
-        image3.style({
-            'image-rendering': 'pixelated'
-        });
+        // http://phrogz.net/tmp/canvas_image_zoom.html
+        image3
+          .attr('style', 'image-rendering: optimizeSpeed; image-rendering: -o-crisp-edges; image-rendering: -webkit-optimize-contrast; image-rendering: optimize-contrast; image-rendering: crisp-edges; image-rendering: pixelated;');
     });
 };
